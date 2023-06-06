93847ff1bfd7a8a277fe5854c1d281593aa899b6
Jay <thellanajr@gmail.com>
2023-06-06 13:46:44 +0800

    enabled event listener on input. Added references for mouseover

--- etchasketch.js
+++ etchasketch.js
@@ -1,13 +1,33 @@
-let board = document.querySelector('.board');
+//make a function to populate the board with squares of a given size
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    // Set the grid template columns and rows to create a grid layout
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-//make 16 columns and each column have width of 1/16 of the width of container. Same for rows
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
-
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
-    board.insertAdjacentElement('beforeend', square);
+    let amount = size * size;  // Calculate the total number of squares
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div'); // Create a new square element
+        square.addEventListener('mouseover', colorSquare); // Add a mouseover event listener
+        square.style.backgroundColor = 'white'; // Set the initial background color of the square
+        board.insertAdjacentElement('beforeend', square); // Add the square to the board
+    }
 }
 
-console.log(document.body);
+populateBoard(16);
+
+//change the size of the board linked with input html
+function changeSize(input) {
+    if(input >= 2 && input <= 100) {
+        populateBoard(input);
+    } else {
+        console.log('Please enter a number between 2 and 100');
+    }
+}
+
+//TODO Function to change the background color of a square to black
+function colorSquare() {
+    this.style.backgroundColor = 'black';
+}
